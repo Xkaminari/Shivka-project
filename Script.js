6b22c164d6c0da57da8a811169f4659c036af3b9
Xkaminari <chahbounmedyassine@gmail.com>
2022-09-25 15:06:11 +0100

    S2v&

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
 console.log("je fonctionne !")
+
+var BgCercles = document.getElementById("BgCercles");
+
+document.onmousemove = function() {
+    var x = event.pageX / 50;
+    var y = event.pageY / 50;
+
+    console.log("positionX" + x + " positionY" + y)
+
+    BgCercles.style.transform = "translate(-" + x + "px,-" + y + "px)";
+}
